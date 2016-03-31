8a001380c74b03f5c7146e9a51f6587efa2e9324
Khushali Bhatt <khushalibhatt1307@yahoo.com>
2016-03-30 21:52:49 -0700

    Moved test class to separate package

---
+++ Security_RuleEngine/src/Test/SecurityRuleEngineTest.java
@@ -0,0 +1,59 @@
+package Test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import FinanceSystem.Corporation;
+import FinanceSystem.Equity;
+import FinanceSystem.Government;
+import FinanceSystem.Issuer;
+import SecurityRuleEngine.DividendRule;
+import SecurityRuleEngine.IssuerRule;
+import SecurityRuleEngine.RuleEngine;
+
+public class SecurityRuleEngineTest {
+
+	@Test
+	public void testDividenRulePositive() {
+		Equity equity = new Equity();
+		equity.setDividend(100);
+		RuleEngine ruleEngine = new RuleEngine(new DividendRule(equity));
+		assertTrue(ruleEngine.processSecurity());
+	}
+
+	@Test
+	public void testDividenRuleNegative() {
+		Equity equity = new Equity();
+		equity.setDividend(50);
+		RuleEngine ruleEngine = new RuleEngine(new DividendRule(equity));
+		assertFalse(ruleEngine.processSecurity());
+	}
+
+	@Test
+	public void testIssuerRulePositive() {
+		Issuer issuer = new Government("US GOVERNMENT");
+		Equity equity = new Equity();
+		equity.setIssuer(issuer);
+		RuleEngine ruleEngine = new RuleEngine(new IssuerRule(equity));
+		assertTrue(ruleEngine.processSecurity());
+	}
+
+	@Test
+	public void testIssuerRuleNegative() {
+		Issuer issuer = new Government("INDIAN GOVERNMENT");
+		Equity equity = new Equity();
+		equity.setIssuer(issuer);
+		RuleEngine ruleEngine = new RuleEngine(new IssuerRule(equity));
+		assertFalse(ruleEngine.processSecurity());
+	}
+	
+	@Test
+	public void testIssuerRuleNegative2() {
+		Issuer issuer = new Corporation("Facebook Inc.");
+		Equity equity = new Equity();
+		equity.setIssuer(issuer);
+		RuleEngine ruleEngine = new RuleEngine(new IssuerRule(equity));
+		assertFalse(ruleEngine.processSecurity());
+	}
+
+}
